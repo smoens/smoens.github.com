d8683521edc139a9e5c444dbe96b79dcabe6bccf
smoens <sarah_m@telenet.be>
2021-05-08 21:27:39 +0200

    add additional info about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,8 @@ const AboutPage = () => {
             <Head title="About" />
             <img src={Me} alt="Sarah Moens on a mountaintop in Canada"/>
             <p>Hello there! I'm Sarah Moens.</p>
-            <p>You will mostly find me talking about data integration, data warehousing, data visualisation, human or machine learning, hiking, soccer, or some new tools or features I recently stumbled upon.</p>
+            <p>I am data engineer at De Lijn, a public transport company in Belgium, where I am focused on data integration projects, data warehousing solutions and data analytics. In addition I provide data visualisation support and manage our current PureData for Analytics (Netezza) database servers.</p>
+            <p>You will mostly find me talking about data integration, data warehousing, data visualisation, human or machine learning, hiking, soccer, or some new tools or features I recently stumbled upon. But always open and happy to learn about other new things!</p>
 
             <ul className={contactStyles.contact}>
                 <li><StaticImage src="../images/twitter.png" alt="Twitter icon"/><a href="https://www.twitter.com/s_moens" target="_blank">s_moens</a></li>
